b73bb00e3b7b42e163e677f9649056b91386591f
esroleo <esroleo@gmail.com>
2021-03-11 22:09:29 -0500

    see post and leave comment completed

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -158,6 +158,7 @@ router.delete('/:id', (req, res) => {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
+      
       res.json(dbUserData);
     })
     .catch(err => {
