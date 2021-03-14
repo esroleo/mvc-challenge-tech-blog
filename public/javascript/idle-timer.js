5b113ee9845c25a97a341cf805a584d654ff9beb
esroleo <esroleo@gmail.com>
2021-03-14 15:51:44 -0400

    minor comment changes

--- public/javascript/idle-timer.js
+++ public/javascript/idle-timer.js
@@ -25,14 +25,10 @@ function idleLogout() {
           if (response.ok) {
             //console.log("response of function log out good")
             alert("Logging you out")
-           // document.location.replace('/login');
             document.location.replace('/');
           } else {
-            //console.log("response of function log out bad")
-            //document.location.replace('/');
-            //document.location.replace('/login');
             alert("You are already logged out")
-            //alert(response.statusText);
+
           }
 
           
