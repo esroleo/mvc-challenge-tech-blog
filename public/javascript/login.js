098a9198bcf5c8b830491ca1d09f48ee577937ee
esroleo <esroleo@gmail.com>
2021-03-14 16:18:13 -0400

    validation added to login page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,34 +5,52 @@
 // Log in logic
 
 async function loginFormHandler(event) {
-    event.preventDefault();
-    console.log("botton clicked")
-  
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-          // go back to homepage after loggin in
-          // updated - go to create dashboard after logging in
-        //document.location.replace('/');
-        //document.location.replace('/dashboard');
-        document.location.replace('/');
-      } else {
-        // alert(response.statusText);
-        alert("Username or password incorrect, please try again.");
+
+  event.preventDefault();
+  console.log("botton clicked")
+
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+    if (username !== "" && password !== "") {
+
+      if (username && password) {
+        const response = await fetch('/api/users/login', {
+          method: 'post',
+          body: JSON.stringify({
+            username,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            // go back to homepage after loggin in
+            // updated - go to create dashboard after logging in
+          //document.location.replace('/');
+          //document.location.replace('/dashboard');
+          document.location.replace('/');
+        } else {
+          // alert(response.statusText);
+          alert("Username/password combination does not exist, please try again or sign up instead.");
+        }
       }
+    } else {
+
+      if (username === "" && password === "") {
+        alert("Please enter a username and password, then submit")
+      } else if (username === "") {
+        alert("Please enter a username, then submit")
+      } else if (password === "") {
+        alert("Please enter a password, then submit")
+      } else {
+        // Do nothing
+      }
+
     }
-  }
+}
+
+  
   
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
